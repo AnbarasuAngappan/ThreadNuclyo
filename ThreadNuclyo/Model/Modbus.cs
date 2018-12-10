a5542e7833dfff08ee8c2b4cc5b2a43fd70eec5a
Anbarasu Angappan <kanbuarasu27@gmail.com>
2018-12-10 13:59:39 +0530

    Add project files.

---
+++ ThreadNuclyo/Model/Modbus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadNuclyo.Model
+{
+    class Modbus
+    {
+        public byte DeviceID { get; set; }
+        public string RiD { get; set; }
+        public string Address { get; set; }
+        public int Quantity { get; set; }
+    }
+}
